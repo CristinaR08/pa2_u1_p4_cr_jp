ed1fdee6d00cf4fd65f62cba501f10b8b9344753
CristinaR08 <cris.04082009@hotmail.com>
2023-05-15 19:40:39 -0500

    taller4

---
+++ src/main/java/com/example/demo/Pa2U1P4CrJpApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P4CrJpApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P4CrJpApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer proyecto");
+		/*Spring framework: Framework que permite la construccion/desarrollo
+		 * de aplicaciones empresariales basadas en Spring*/
+		
+		Profesor profe = new Profesor();
+		profe.setApellido("RIVERA, AGUAS");
+		profe.setCedula("1724340391");
+		profe.setFechaNacimiento(LocalDateTime.now());
+		profe.setNombre("Cristina, Roberto");
+		
+		System.out.println(profe);
+	}
+
+}
